c2735ebe33d3c1310c6a8d445488f437a0a98e66
Ruslan <rzhitkovich@bk.ru>
2023-02-28 13:13:30 +0300

    Add views and urls Add static files Add base template and extend other from the main Add navbar to base.html from bootstrap

--- shop/config/settings.py
+++ shop/config/settings.py
@@ -1,4 +1,4 @@
-
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -101,7 +101,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
