c2060274a06616ff0bf11d888a3a3ce8054efd11
Ruslan <rzhitkovich@bk.ru>
2023-03-09 18:50:29 +0300

    Make some logic in checkout.html: - if you have only digital item in your cart website won't show you shipping form.
    It works by changes in models.py and also by js.scrypth which was inserted in checkout.html because we use it only in this page.

--- shop/store/models.py
+++ shop/store/models.py
@@ -2,55 +2,58 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Customer(models.Model):             # Клиент
-    user = models.OneToOneField(User, null = True, blank = True, on_delete=models.CASCADE)
-    name = models.CharField(max_length=200, null = True)
-    email = models.CharField(max_length=200, null = True)
+# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
-    class Meta:
-        verbose_name = 'Заказчик'
-        verbose_name_plural = 'Заказчики'
 
-
-class Product(models.Model):           # Продукт который покупает клиент
-    name = models.CharField(max_length=200,null = True)
+class Product(models.Model):
+    name = models.CharField(max_length=200)
     price = models.FloatField()
-    digital = models.BooleanField(default = False, null = True, blank = False)
+    digital = models.BooleanField(default=False, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
     def __str__(self):
         return self.name
 
-    @property                   # пишем эту функцию в html при выводе фото
-    def imageURL(self):         # без этой функции при отсутствии фото на какой-нибудь из записей
-        try:                    # при попытке запуска сервера будет бросаться исключение
+    @property
+    def imageURL(self):
+        try:
             url = self.image.url
         except:
             url = ''
         return url
 
-    class Meta:
-        verbose_name = 'Товар'
-        verbose_name_plural = 'Товары'
 
-
-class Order(models.Model):            # Заказ
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null = True, blank = True)
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
-    complete = models.BooleanField(default=False, null = True, blank=False)
-    transaction_id = models.CharField(max_length=200, null = True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
 
     def __str__(self):
         return str(self.id)
 
     @property
+    def shipping(self):
+        shipping = False
+        orderitems = self.orderitem_set.all()
+        for i in orderitems:
+            if i.product.digital == False:
+                shipping = True
+        return shipping
+
+    @property
     def get_cart_total(self):
-        orderitems = self.orderitem_set.all()   # получаем querrySet
+        orderitems = self.orderitem_set.all()
         total = sum([item.get_total for item in orderitems])
         return total
-
 
     @property
     def get_cart_items(self):
@@ -59,53 +62,26 @@ class Order(models.Model):            # Заказ
         return total
 
 
-
-
-
-    class Meta:
-        verbose_name = 'Заказ'
-        verbose_name_plural = 'Заказы'
-
-
-class OrderItem(models.Model):          # Единица заказа, т.е сам продукт но с количеством
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null = True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL,null = True)
-    quantity = models.IntegerField(default=0, null = True, blank=True)
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return str(self.id)
 
     @property
     def get_total(self):
-        return self.product.price * self.quantity
-
-
-    class Meta:
-        verbose_name = 'Составляющее заказа'
-        verbose_name_plural = 'Составляющeе заказа'
-
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingAdress(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank = True, null = True)
-    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank = True, null = True)
-    name = models.CharField(max_length=200, null = True)
-    address = models.CharField(max_length=200, null=True)
-    city = models.CharField(max_length=200, null=True)
-    zipcode = models.CharField(max_length=200, null=True)
-    state = models.CharField(max_length=200, null=True)
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.address
-
-    class Meta:
-        verbose_name = 'Адрес'
-        verbose_name_plural = 'Адреса'
-
-
-
-
-
-
--- shop/store/views.py
+++ shop/store/views.py
@@ -6,8 +6,18 @@ from store.models import *
 
 
 def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,complete=False)
+        items = order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
+        cartItems = order['get_cart_items']
+
     products = Product.objects.all()
-    context = {'products':products}
+    context = {'products':products, 'cartItems':cartItems}
     return render(request, 'store/store.html', context)
 
 def cart(request):
@@ -18,7 +28,7 @@ def cart(request):
         items = order.orderitem_set.all()
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
     context = {'items':items, 'order':order}
     return render(request, 'store/cart.html', context)
 
@@ -29,7 +39,7 @@ def checkout(request):
         items = order.orderitem_set.all()
     else:
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping':False}
     context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
 
